e238e6900d49f05aba9078c506064df7272d4c00
falkomerr <105551615+falkomerr@users.noreply.github.com>
2024-04-07 16:02:25 +0300

    build: add entities and features folders like in fsd methodology

--- src/app/+page.svelte
+++ src/app/+page.svelte
@@ -2,4 +2,9 @@
   import { HomePage } from '@/pages/home';
 </script>
 
+<svelte:head>
+  <meta name="title" content="Starter for SvelteKit" />
+  <meta name="description" content="Starter for SvelteKit with FSD, shadcn-svelte and tailwind" />
+</svelte:head>
+
 <HomePage />
--- src/widgets/header/ui/header.svelte
+++ src/widgets/header/ui/header.svelte
@@ -1,3 +1,3 @@
 <header class="flex w-screen items-center justify-center py-4">
-  <h1 class="w-fit text-3xl">Starter for sveltekit with FSD, shadcn-svelte and tailwind</h1>
+  <h1 class="w-fit text-3xl">Starter for SvelteKit with FSD, shadcn-svelte and tailwind</h1>
 </header>
